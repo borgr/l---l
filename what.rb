16ca905187825889d06e881bfb9ce2cc1f14ec08
borgr <borgr@users.noreply.github.com>
2014-04-21 19:19:28 +0300

    Update what.rb
    added properPhrase method, that deletes phrases like [ 3 . "4" etc.

--- what.rb
+++ what.rb
@@ -17,6 +17,12 @@ RegNoWhat = /#{NoWhat.join("|")}/ # assuming there are no special chars
 # exact - if true returns only if the full string is in the array
 # note that the check is case sensitive
 def in_arr? (arr, text, exact)
+
+# ###how come these question is being made? what is the bug?
+# *****************
+# question62: what are associated with behavioral changes characteristic of depression, demonstrating the correlation between the olfactory bulb and emotion? ---
+# answer: These hippocampal changes due to olfactory bulb removal
+
 	if exact
 		return arr.include? text
 	else
@@ -54,36 +60,42 @@ def tagged (phrase, tags, ignore, insensitive = true, exact = true)
 	return nil
 end
 
+#makes sure the phrase is proper
+def properPhrase (phrase)
+	return phrase.words.any?{|word| word.tag != "CD"}
+end
+
 #ask what verb
 #answer noun phrase before the verb
 counter = 0;
 par.each_sentence do |sent|
 	verb,noun = nil,nil
 	sent.each do |phrase|
-		if phrase.tag == "VP" 
-			#check if this is not a special case of VP
-			if !(RegNoWhat === phrase.to_s)
-				verb = phrase
-			else
-				#need to implement or use another loop
-			end
+		if properPhrase(phrase)
+			if phrase.tag == "VP" 
+				#check if this is not a special case of VP
+				if !(RegNoWhat === phrase.to_s) && 
+					verb = phrase
+				else
+					#need to implement or use another loop
+				end
 
-		 #english is usually svo -> subject verb object 
-		 #so if a name phrase is before the verb it will be probably the subject
-		elsif phrase.tag == "NP" && verb == nil
-			check_tags = tagged(phrase, ["DT","PRP"], ["the", "a"])
-			# puts check_tags.to_s
-			if !check_tags
-				noun = phrase
+			 #english is usually svo -> subject verb object 
+			 #so if a name phrase is before the verb it will be probably the subject
+			elsif phrase.tag == "NP" && verb == nil
+				check_tags = tagged(phrase, ["DT","PRP", "EX"], ["the", "a"])
+				# puts check_tags.to_s
+				if !check_tags
+					noun = phrase
+				end
+			end
+			if verb != noun && verb != nil && noun != nil
+				counter += 1
+				puts "question#{counter}: what #{verb}? ---"
+				puts "answer: #{noun}"
+				puts "*****************"
 			end
 		end
 	end
-	if verb != noun && verb != nil && noun != nil
-		counter += 1
-		puts "question#{counter}: what #{verb}? ---"
-		puts "answer: #{noun}"
-		puts "*****************"
-	end
-
 end
 
