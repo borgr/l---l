5dffa49ddedd937b400320b5d0fd55e5fbc866d0
borgr <borgr@users.noreply.github.com>
2014-04-24 08:39:49 +0300

    Update what.rb

--- what.rb
+++ what.rb
@@ -1,69 +1,17 @@
-
 require 'treat'
 
 include Treat::Core::DSL
 
+require_relative 'HelpQuestions'
 #d = document "http://en.wikipedia.org/wiki/Olfactory_bulb"
 
 #d.apply(:chunk,:segment,:tokenize,:tag)
 
-par = document 'processed_doc.xml'
+par = get_document
 
 #a constant list of prepositions that should not be in the verb phrase
 NoWhat = ['into']
 RegNoWhat = /#{NoWhat.join("|")}/ # assuming there are no special chars
-
-# checks if the string is in the array or is part of the array's strings
-# exact - if true returns only if the full string is in the array
-# note that the check is case sensitive
-def in_arr? (arr, text, exact)
-
-# ###how come these question is being made? what is the bug?
-# *****************
-# question62: what are associated with behavioral changes characteristic of depression, demonstrating the correlation between the olfactory bulb and emotion? ---
-# answer: These hippocampal changes due to olfactory bulb removal
-
-	if exact
-		return arr.include? text
-	else
-		return arr.any?{|s| s.include? text}
-	end
-end
-
-# a function that checks if one of the sections in the phrase has one of the tags
-# the function ignores matches that are in the ignore array
-# phrase - any section
-# tags - an array of strings containing POS tag names
-# ignore - an array with strings to ignore
-# insensitive - boolean case insensitive or not. default is true.
-# exact - check for exact match
-def tagged (phrase, tags, ignore, insensitive = true, exact = true)
-	# make sure insensitive
-	if insensitive
-		ignore = ignore.map(&:downcase)
-	end
-	# pos are always upcase, just to maek sure
-	tags = tags.map(&:upcase)
-
-	phrase.each do |sub|
-		if tags.include? sub.tag
-			if insensitive
-				if !in_arr?(ignore, sub.to_s.downcase, exact)
-					return sub
-				end
-			elsif !in_arr?(ignore, sub, exact)
-					return sub
-				
-			end
-		end
-	end
-	return nil
-end
-
-#makes sure the phrase is proper
-def properPhrase (phrase)
-	return phrase.words.any?{|word| word.tag != "CD"}
-end
 
 #ask what verb
 #answer noun phrase before the verb
