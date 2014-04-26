cb3795a4c7952db4de477b017e60d30d406fdae3
borgr <borgr@users.noreply.github.com>
2014-04-26 17:04:04 +0300

    Update HelpQuestions.rb
    added arr_in_phrase?
    made a lot of progress on remove_interrupts

--- HelpQuestions.rb
+++ HelpQuestions.rb
@@ -16,6 +16,13 @@ NotPhrase = ["punctuation", "symbol", "number"]
 		end
 	end
 
+	# checks if any of the strings in the array is in the string
+	def arr_in_phrase? (arr, string)
+		#might be unefficient to recreate the reg each time, maybe better to return a regex?
+		reg = arr.map{|str| Regexp.escape(str)}
+		reg = /#{arr.join("|")}/ 
+		return reg === string
+	end
 	# a function that checks if one of the sections in the phrase has one of the tags
 	# the function ignores matches that are in the ignore array
 	# phrase - any section
@@ -78,34 +85,36 @@ NotPhrase = ["punctuation", "symbol", "number"]
 		return words.any? {|word| phrase_string.start_with? (word)} && !ignore.any? {|word| phrase_string.start_with? (word)}
 	end
 
-#######################################need to debug!! is_a in wrong syntax twice####################################################################################
-	# This function gets a phrase and returns a string of the phrase without any HESGER
+	# If change is true, changes the phrase into a sentence
+	# sentence - a string that may need to become a sentence.
+	def sentencize (sentence, change)
+		if sentence
+			sentence.capitalize!
+		end
+		return sentence
+	end
+
+	# This function gets a phrase and returns a string of the phrase without any interrupting phrase
 	# phrase - an entity to check (preferable not tokens)
 	# start - a boolean representing if this is the beggining of a phrase or comes
 	# after a punctuation
 	# sentence - true if this is a sentence and not just a phrae
-	def remove_ads(phrase, sentence = true, start = true)
-		res = remove_ads_recursivly(phrase, start, phrase.to_s)
-		if sentence
-			res.capitalize!
-		end
-		return res
+	def remove_interrupts(phrase, sentence = true, start = true)
+		res = remove_interrupts_recursivly(phrase, start, phrase.to_s)
+		return sentencize(res, sentence)
 	end
 
-	# This function goes through each level of the phrase and removes HESGER from it
+	# This function goes through each level of the phrase and removes interrupting phrase from it
 	# 
-	def remove_ads_recursivly(phrase, start = true, string)
+	def remove_interrupts_recursivly(phrase, start = true, string)
 		check_punctuation = false
 		first_punctuation = nil
 		punctuation_before = start
 		tags = ["ADVP"]
 		last_phr = nil #the variable should never be used before a real string is assigned to it
 		phrase.each do |sub|
-			if check_punctuation && sub.is_a?(Treat::Entities::Punctuation) # if it is a panctuation remove HESGER
-				puts "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin"+phrase
-				puts "sssssssssssssssssssssssssssssssssssssssssssssssstring " + phrase.to_s
+			if check_punctuation && sub.is_a?(Treat::Entities::Punctuation) # if it is a panctuation remove interrupting phrase
 				string.gsub!(/\s* #{Regexp.escape(first_punctuation)}* \s* #{Regexp.escape(last_phr)} \s* #{Regexp.escape(sub.to_s)} \s*/x, " ")
-				puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaastring after " + last_phr
 				check_punctuation = false
 				punctuation_before = true
 				first_punctuation = sub.to_s
@@ -114,22 +123,22 @@ NotPhrase = ["punctuation", "symbol", "number"]
 				if punctuation_before && exact_tag(sub, tags)
 					check_punctuation = true
 					last_phr = sub.to_s
-				# check if next run will have the potential of being an HESGER
+				# check if next run will have the potential of being an interrupting phrase
 				elsif sub.is_a?(Treat::Entities::Punctuation)
 					punctuation_before = true
 					first_punctuation = sub.to_s
 				else 
-				# This sub is neither a punctuation nor a potential HESGER
+				# This sub is neither a punctuation nor a potential interrupting phrase
 					check_punctuation = false
 					punctuation_before = false
 				end
 			end
 
 			# do the same for sub entities
-			string = remove_ads_recursivly(sub, check_punctuation, string)
+			string = remove_interrupts_recursivly(sub, check_punctuation, string)
 		end
 
-		#if it ends with it, and without a punctuation, it must be an HESGER at the end.
+		#if it ends with it, and without a punctuation, it must be an interrupting phrase at the end.
 		if check_punctuation
 			string.gsub!(/\s* #{Regexp.escape(first_punctuation)}* \s* #{Regexp.escape(last_phr)} \s* /x, " ")
 			
@@ -137,19 +146,67 @@ NotPhrase = ["punctuation", "symbol", "number"]
 		return string
 	end
 
-	# # need to check if it works!!!!! and then delete this part############################################################
+	# removes text with the same position in the phrase (a beautiful and merciful > a beautiful)
+	# an entity 
+	# important - an array of strings phrases containing strings in important will not be removed
+	def remove_duplicates(phrase, important, sentence = true)
+		res = remove_duplicates_recursivly(sentence, important, sentence.to_s)
+		return sentencize(res, sentence)
+	end
+
+	# a recursive helper for remove_duplicates
+	# string - the string we are working at
+	# important - an array of strings phrases containing strings in important will not be removed
+	# phrase
+	def remove_duplicates_recursivly (phrase, important, string)
+		type = [""]
+		removables = ["CC", ","]
+		phrase.each do |sub|
+			if sub.exact_tag(type)
+				# it is a repetition
+				if delete.empty?
+				# If it is the first repetition, check the first
+				# done to avoid checking every entity if in important
+					delete[!arr_in_phrase(important, sub.to_s)]
+				end
+				# save it
+				same << sub.to_s
+				delete << !arr_in_phrase(important, sub.to_s)
+
+			elsif sub.exact_tag(removables)
+				# it is not a repetition, but it is a connector of some sort ("," "or" "and" maybe something else?)
+				to_remove = sub.to_s
+			elsif !sub.exact_tag(removables)
+				# not a repetition
+				if !same.empty? && delete.any?{|boolean|boolean}
+					# delete unnecessary information
+					# need to implement: sub! every true and its left removable (from to_remove) and spaces by a space
+					# if there is only one true, delete all removables
+					# elsif the last one is false put its left removable (from to_remove)
+					# before the last undeleted string, and delete the other removable so there wont be 2 of them. (example: one, two and three -> delete three -> one and two)
+				end
+				same = [sub.to_s]
+				delete = []
+				if sub.has? :tag
+					type = sub.tag
+				end
+			end
+			string = remove_duplicates_recursivly(sub, important, string)
+		end
+	end
+
 	# require 'treat'
 	# include Treat::Core::DSL
 	# # d = section 'In humans, however, the olfactory bulb is on the inferior (bottom) side of the brain.'
 	# # d.apply(:chunk,:segment,:tokenize,:parse)
 	# d = get_document
 	# # puts d.visualize
-	# remove_ads(d)
+	# remove_interrupts(d)
 	###############################################################################################################
 
-	# takes a sentence and removes unimportant parts (X ,y and Z -> X , HESGER), returns a string
+	# takes a sentence and removes unimportant parts (X ,y and Z -> X , interrupting phrase), returns a string
 	# important - an array of strings. when removing duplicants, chooses the important ones if they exist.
 	def compact_sentence (sentence, important)
-		###############unimplemented, choosing important may work like remove_ads (go each level recursivly, and remove if necessary)
-		remove_ads(sentence)
+		remove_interrupts(sentence)
+		remove_duplicates(sentence, important)
 	end
