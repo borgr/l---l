f7fdd5ed9d3251ee462252c67689eb079557b7b3
borgr <borgr@users.noreply.github.com>
2014-08-13 11:24:50 +0300

    code added
    creates simple questions now, many work nicely.
    like everything right now, many tweaks are needed

--- where.rb
+++ where.rb
@@ -3,7 +3,7 @@ require 'treat'
 include Treat::Core::DSL
 
 require_relative 'HelpQuestions'
-
+IN_IGNORE_LIST = ["in time", "in particular"]
 par = get_document
 #ask where ************
 #answer noun ************
@@ -13,11 +13,24 @@ par.each_sentence do |sent|
 	#need to check which sentences contain PP with the word in at the begginning
 	#then to see if they are not in time or something. 
 	prepositional = tagged(sent, ["PP"])
-	if phrase_start_with?(prepositional, false, ["in time", "in particular"], "in ") 
-	##########currently finding good sentences, probably ignore list should be bigger.
-	######## need to make it ask "where does compact_sentence without PP" and answer is PP
-			putsputs prepositional
+
+	if phrase_start_with?(prepositional, false, IN_IGNORE_LIST , "in ") 
+		counter += 1
+		result = sent.to_s
+		result.slice! prepositional.to_s
+		puts "question#{counter}: where does " + result + "?---"
+		puts "answer: #{prepositional}"
+		puts "*****************"
+	
 	end
 end
 
-#################not working, need to find sentences that contain a PP phrase that starts with in
+=begin
+problems: 
+double space or , remains
+. at end of sentence remains, should make a general question-answer making function (that also removes what is needed)
+some questions sound better starting with "in what" instead of "where"
+
+	bad questions found
+
+end
